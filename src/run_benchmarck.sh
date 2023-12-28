25830039ff4c6c500a50e32be5283aa748be346c
owos <owodunniabraham@gmail.com>
2023-12-28 18:19:25 +0000

    update paths to csv

--- src/run_benchmarck.sh
+++ src/run_benchmarck.sh
@@ -49,7 +49,7 @@ for w_model in ${whisper_models_list[@]};
 
 #!/bin/bash
 
-CSV_paths=("path/to/csv1" "data/" "data/" ) # List of CSV paths
+CSV_paths=("data/val_9aug23_43432-all_data_augs_procs_filtered-42934-clean-samplecols.csv" "data/intron-dev-public-3231-clean.csv" "data/intron-test-public-6346-clean.csv" "data/intron_fresh_audio-test-prod-2023_09_14_23_33_24_496601_local.csv" "data/personalizations_2023-07-25_local.csv") # List of CSV paths
 audio_paths=("/data/data/" "/data/data/intron/" "/data/data/intron/" "/data/data/prod/" "/data/data/personalize2") # List of audio paths
 
 for ((i=0; i<${#CSV_paths[@]}; i++)); do
