0c92bb2f299e52b32145124ab05866de7008cdce
owos <owodunniabraham@gmail.com>
2024-04-05 09:02:04 +0000

    update inference analysis

--- run_benchmark_rerun.sh
+++ run_benchmark_rerun.sh
@@ -6,17 +6,17 @@ models_list=( # "openai/whisper-large-v3" \
             #"distil-whisper/distil-large-v3" \
             # "distil-whisper/distil-large-v2" \
             "/data4/abraham/training_with_new_sampler/AfriSpeech-Dataset-Paper/src/experiments/parakeet_afrispeech_benchmark_100e_rerun_ctc/Model-en.nemo" \
-            "/data4/abraham/training_with_new_sampler/AfriSpeech-Dataset-Paper/src/experiments/parakeet_afrispeech_benchmark_100e/ctc_Model-en.nemo" \  
+            #"/data4/abraham/training_with_new_sampler/AfriSpeech-Dataset-Paper/src/experiments/parakeet_afrispeech_benchmark_100e/ctc_Model-en.nemo" \  
             # "openai/whisper-large-v2" \ 
             # "/data4/abraham/training_with_new_sampler/AfriSpeech-Dataset-Paper/src/experiments/whisper_small_afrispeech_10e_lora/checkpoints" \ 
             # "/data4/abraham/training_with_new_sampler/AfriSpeech-Dataset-Paper/src/experiments/whisper_small_afrispeech_10e/checkpoints"  \
-            "/data4/abraham/training_with_new_sampler/AfriSpeech-Dataset-Paper/src/experiments/parakeet_ctc_afrispeech_benchmark_e40/Model-en.nemo" \
-            "nvidia/parakeet-rnnt-1.1b" \
-            "nvidia/canary-1b" \
+            #"/data4/abraham/training_with_new_sampler/AfriSpeech-Dataset-Paper/src/experiments/parakeet_ctc_afrispeech_benchmark_e40/Model-en.nemo" \
+            #"nvidia/parakeet-rnnt-1.1b" \
+            #"nvidia/canary-1b" \
             # "openai/whisper-small" \ 
             # "/data4/saved_models/w2v_robust_200k_prod" \
             # "/data4/saved_models/lg_robust_500k_steps" \
-            "nvidia/parakeet-ctc-0.6b" \
+            #"nvidia/parakeet-ctc-0.6b" \
             # "openai/whisper-medium" \
             # "facebook/wav2vec2-large-robust-ft-libri-960h" \
             )
